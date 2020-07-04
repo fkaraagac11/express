6378bce825e60a4ec5efa6e736a683a3e551d120
fkaraagac11 <fehmi11.qa@gmail.com>
2020-07-03 22:15:12 -0400

    update

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,12 +1,25 @@
+// const express = require("express");
+
+// //init express
+// const app = express();
+
+// // create the endpoints/ route handlers
+
+// app.get("/", (req, res) => {
+//     res.send("Hello Melih how are you?");
+// });
+
+// console.dir(app.locals.title);
+// app.listen(8888);
+
 const express = require("express");
-
-//init express
 const app = express();
+const port = 4444;
 
-// create the endpoints/ route handlers
+app.use("/static", express.static("static"));
 
-app.get("/", (req, res) => {
-    res.send("Hello Melih how are you?");
-});
+app.get("/", (req, res) => res.send("Hello Worldsss!"));
 
-app.listen(8888);
+app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`)
+);
